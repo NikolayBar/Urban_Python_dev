c1c72285bfe444d706e9764c7d98b6380fe6277b
NikolayBar <nikolbar@yandex.ru>
2024-07-26 22:39:27 +0300

    добавлено решение по 3 пункту

--- modul_3/module_3_3.py
+++ modul_3/module_3_3.py
@@ -4,6 +4,7 @@ def print_params(a=1, b='строка', c=True):
 
 values_list = [1, 'string', [3, 4, 5]]
 values_dict = {'a': 1, 'b': True, 'c': [6, 7, 8]}
+values_list_2 = [54.32, 'Строка']
 
 print_params()
 print_params(11, 22)
@@ -13,3 +14,4 @@ print_params(c=[1, 2, 3])
 
 print_params(*values_list)
 print_params(**values_dict)
+print_params(*values_list_2, 42)
