17e1ed2c7522553d671f03a8e3ec3df9b5329546
NikolayBar <nikolbar@yandex.ru>
2024-07-26 17:55:31 +0300

    add module_3_3.py

---
+++ modul_3/module_3_3.py
@@ -0,0 +1,15 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+values_list = [1, 'string', [3, 4, 5]]
+values_dict = {'a': 1, 'b': True, 'c': [6, 7, 8]}
+
+print_params()
+print_params(11, 22)
+print_params(111, 222, 333)
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+print_params(*values_list)
+print_params(**values_dict)
