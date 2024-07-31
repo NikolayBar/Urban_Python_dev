2c77988fec0c5f623e0f82b05e7a6c8b3a725205
NikolayBar <nikolbar@yandex.ru>
2024-07-31 20:00:00 +0300

    оптимизация module2hard.py

--- modul_2/module2hard.py
+++ modul_2/module2hard.py
@@ -1,14 +1,12 @@
 def get_password(in_num: int) -> str:
     res = ''
-    for i in range(1, in_num):
-        for j in range(i, in_num):
-            if i == j:
-                continue
+    for i in range(1, in_num // 2 + 1):
+        for j in range(i + 1, in_num):
             if in_num % (i + j) == 0:
                 res += str(i) + str(j)
     return res
 
 
-n = int(input())
-result = get_password(n)
-print(result)
+# n = int(input())
+# result = get_password(n)
+# print(result)
---
+++ test.py
@@ -0,0 +1,8 @@
+from dis import dis
+
+
+def fn():
+    print('I like Python')
+
+
+dis(fn)
