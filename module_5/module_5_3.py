8ad2ca8cf58e4ce83883238b05271c406cdd64e7
NikolayBar <nikolbar@yandex.ru>
2024-08-10 19:37:57 +0300

    add module_5_3.py, test_module.py

---
+++ module_5/module_5_3.py
@@ -0,0 +1,52 @@
+class House:
+
+    def __init__(self, name: str, floors: int) -> None:
+        self.name = name
+        self.number_of_floors = floors
+
+    def go_to(self, floor: int) -> None:
+        if not (1 <= floor <= self.number_of_floors):
+            print('Такого этажа не существует')
+            return
+        for lv in range(1, floor + 1):
+            print(lv)
+
+        return None
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.__eq__(other) or self.__lt__(other)
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.__eq__(other) or self.__gt__(other)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            self.number_of_floors += other
+            return self
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __iadd__(self, other):
+        return self.__add__(other)
