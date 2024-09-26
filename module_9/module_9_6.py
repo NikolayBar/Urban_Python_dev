4c718978d95286bf958963b5f391655b0e1d8e63
NikolayBar <nikolbar@yandex.ru>
2024-09-26 16:40:03 +0300

    add module_9_6.py

--- module_9/module_9_6.py
+++ module_9/module_9_6.py
@@ -1,21 +1,17 @@
-def all_variants(text, reverse=False):
-    if not reverse:
-        index, k = 0, 1
-    else:
-        index, k = len(text)-1, -1
-    while 0 <= index < len(text):
-        yield text[index]
-        index += k
+def all_variants(text, *, num_chars=1, start=0):
+    num_chars = num_chars if num_chars > 0 else 1
+    index = start
+    while num_chars <= len(text):
+        if len(text[index: index + num_chars]) == num_chars:
+            yield text[index: index + num_chars]
+        index += 1
+        if index == len(text):
+            index = start
+            num_chars += 1
 
 
 if __name__ == '__main__':
 
-    a = all_variants("abc")
+    a = all_variants("abcd")
     for i in a:
-        print(i, end=' ')
-    print()
-
-    b = all_variants("1234567890", True)
-    for i in b:
-        print(i, end=' ')
-    print()
+        print(i)
