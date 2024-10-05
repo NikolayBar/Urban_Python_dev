e7be5ca92a5a7fe45cb7e26065485114c299f037
NikolayBar <nikolbar@yandex.ru>
2024-10-05 11:33:38 +0300

    edit module_10_03.py

--- module_10/module_10_03.py
+++ module_10/module_10_03.py
@@ -2,10 +2,11 @@ from threading import Thread, Lock
 from time import sleep
 from random import randint
 
+
 class Bank:
-    TRANS_NUM = 10
+    TRANS_NUM = 100
     NUM_RANGE = 50, 500
-    DELAY = 1
+    DELAY = 0.001
 
     def __init__(self):
         self.balance = 0
@@ -13,26 +14,24 @@ class Bank:
 
     def deposit(self):
         for _ in range(self.TRANS_NUM):
-            if self.balance >= 500 and self.lock.locked():
-                self.lock.release()
             x = randint(*self.NUM_RANGE)
-            self.balance += x
-            print(f'Пополнение: {x}. Баланс: {self.balance}')
-            sleep(self.DELAY)
-
-
+            with self.lock:
+                self.balance += x
+                print(f'Пополнение: {x}. Баланс: {self.balance}')
+                sleep(self.DELAY)
 
     def take(self):
         for _ in range(self.TRANS_NUM):
             x = randint(*self.NUM_RANGE)
-            print(f'Запрос на {x}')
-            if x <= self.balance:
-                self.balance -= x
-                print(f'Снятие: {x}. Баланс: {self.balance}')
-            else:
-                print(f'Запрос отклонён, недостаточно средств, Баланс: {self.balance}')
-                self.lock.acquire()
-        sleep(self.DELAY)
+            with self.lock:
+                print(f'Запрос на {x}')
+                if x <= self.balance:
+                    self.balance -= x
+                    print(f'Снятие: {x}. Баланс: {self.balance}')
+                else:
+                    print(f'Запрос отклонён, недостаточно средств, Баланс: {self.balance}')
+                sleep(self.DELAY)
+
 
 bk = Bank()
 
@@ -45,6 +44,5 @@ for obj in threads:
 
 for obj in threads:
     obj.join()
-
 
 print(f'Итоговый баланс: {bk.balance}')
