83b85d2f5d27f73d857baafd56d217d6c43326b7
NikolayBar <nikolbar@yandex.ru>
2024-10-12 21:30:48 +0300

    final module_10_04.py

--- module_10/module_10_04.py
+++ module_10/module_10_04.py
@@ -3,6 +3,7 @@ from time import sleep
 from random import randint
 import queue
 
+
 class Table:
 
     def __init__(self, number):
@@ -25,12 +26,33 @@ class Cafe:
     def __init__(self, *args):
         self.tables = args
         self.queue = queue.Queue()
+        return
 
-    def guest_arrival(self, *guests):
-        pass
+    def guest_arrival(self, *arg):
+        l_t = len(self.tables)
+        for i, v in enumerate(arg):
+            if tables[i % l_t].guest is None:
+                tables[i % l_t].guest = v
+                print(f'{v.name} сел(-а) за стол номер {tables[i % l_t].number}')
+            else:
+                print(f'{v.name} в очереди')
+                self.queue.put(v)
+        return
 
     def discuss_guests(self):
-        pass
+        while not all(x.guest is None for x in self.tables):
+            for i, v in enumerate(self.tables):
+                if v.guest is not None and not v.guest.is_alive():
+                    print(f'{v.guest.name} покушал(-а) и ушёл(ушла)')
+                    print(f'Стол номер {tables[i].number} свободен')
+                    self.tables[i].guest = None
+
+            for i, v in enumerate(self.tables):
+                if v.guest is None and not self.queue.empty():
+                    g = self.queue.get()
+                    print(f'{g.name}  вышел(-ла) из очереди и сел(-а) за стол номер {v.number}')
+                    g.start()
+                    self.tables[i].guest = g
 
 
 if __name__ == '__main__':
