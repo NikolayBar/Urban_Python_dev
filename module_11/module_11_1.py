88537ac72575d4d2c67e7ce48751aca3342f1321
NikolayBar <nikolbar@yandex.ru>
2024-10-22 17:12:40 +0300

    edit module_11_1.py

--- module_11/module_11_1.py
+++ module_11/module_11_1.py
@@ -10,6 +10,7 @@ im_url = r'https://i.pinimg.com/736x/9f/96/b5/9f96b5dc3700f5a21786cfc91b8f1508.j
 try:
     obj = requests.get(im_url, stream=True).raw
 except requests.exceptions.RequestException as e:
+    print('Невозможно загрузить изображение!')
     sys.exit(1)
 
 try:
@@ -18,9 +19,9 @@ except IOError:
     print('Невозможно открыть изображение!')
     sys.exit(1)
 
-# Обработка
+# Обработка изображения
 x, y = img.size
-kf = 0.5
+kf = 0.75
 x, y = int(x * kf), int(y * kf)
 f_size = int(28 * kf)
 t_shf = int(15 * kf)
@@ -28,9 +29,11 @@ img = img.resize((x,y), reducing_gap=3)
 
 img_draw = ImageDraw.ImageDraw(img)
 text = f'Лисичка\nразмер {x}x{y}'
-font = ImageFont.truetype("arial.ttf", f_size, encoding='utf-8')
+font = ImageFont.truetype("segoepr.ttf", f_size, encoding='utf-8')
 
 img_draw.text((t_shf,t_shf), text, font=font, fill=True)
-img.show()
 
+img.show()
+# Сохранение изображения
+img.save(f'Лисичка_{x}x{y}.png', 'png')
 
