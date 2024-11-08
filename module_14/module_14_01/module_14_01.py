0ef3f070182f89c8c856d29d4c7cf521c9734c4b
NikolayBar <nikolbar@yandex.ru>
2024-11-08 03:01:26 +0300

    module_14_02

--- module_14/module_14_01/module_14_01.py
+++ module_14/module_14_01/module_14_01.py
@@ -21,15 +21,25 @@ cursor.execute("UPDATE Users SET balance=1000")
 cursor.execute("UPDATE Users SET balance=500 WHERE id % 2 = 1")
 
 cursor.execute("SELECT COUNT(id) FROM Users")
-count_row = cursor.fetchall()[0][0]
+count_row = cursor.fetchone()[0]
 
 for i in range(1, count_row + 1, 3):
     cursor.execute(f"DELETE FROM Users WHERE id = {i}")
 
-cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != 60")
-users = cursor.fetchall()
-for el in users:
-    print(f'Имя: {el[0]} | Почта: {el[1]} | Возраст: {el[2]} | Баланс: {el[3]}')
+# cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != 60")
+# users = cursor.fetchall()
+# for el in users:
+#     print(f'Имя: {el[0]} | Почта: {el[1]} | Возраст: {el[2]} | Баланс: {el[3]}')
+
+cursor.execute("DELETE FROM Users WHERE id = 6")
+
+cursor.execute("SELECT COUNT(*) FROM Users")
+total_row = cursor.fetchone()[0]
+
+cursor.execute("SELECT AVG(balance) FROM Users")
+balance_avg = cursor.fetchone()[0]
+
+print(balance_avg)
 
 connection.commit()
 connection.close()
