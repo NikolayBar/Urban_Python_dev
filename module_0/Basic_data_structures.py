74f02f34d1f26e51792059bb372282944bdb1cfc
NikolayBar <nikolbar@yandex.ru>
2025-06-12 17:07:41 +0300

    update gitignore

---
+++ module_0/Basic_data_structures.py
@@ -0,0 +1,24 @@
+# 1st program
+res1 = (9 ** 0.5) * 5
+print(res1)
+
+# 2st program
+num1 = 9.99
+num2 = 1000
+res2 = num1 > 9.98 and num2 != 1000.1
+print(res2)
+
+# 3st program
+num1 = 2 * 2 + 2
+num2 = 2 * (2 + 2)
+res3 = num1 == num2
+print(num1, num2, res3, sep='\n')
+
+# 4th program
+str_num = '123.456'
+res4 = int(float(str_num) * 10) % 10
+print(res4)
+
+
+
+
